4049e06abd1f75fe73fe9ff6214b60d3fff7d47c
Matthew Kramer <kramerm@mail.usf.edu>
2017-03-28 21:55:11 -0400

    adding signup functionality

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 app = Flask(__name__)
 
-@app.route("/")
+# HTML: Home Page
+@app.route('/')
 def main():
 	return render_template('index.html')
 
+# HTML: Sign Up
+@app.route('/showsignup')
+def showsignup():
+	return render_template('signup.html')
+
+# BACKEND: Sign Up Method
+@app.route('/signup')
+def signup():
+	# Read posted values from user interface
+	username = request.form['inputUsername']
+	password = request.form['inputPassword']
+
+	# Validation
+	if username and password:
+		return json.dumps({'html':'<span>Creating account...</span>'})
+	else:
+		return json.dumps({'html':'<span>Error: enter a username and password.</span>'})
+
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True, use_reloader=True)
 	
---
+++ static/js/signup.js
@@ -0,0 +1,20 @@
+$(function()
+{
+	$('#signupbutton').click(function()
+	{
+		$.ajax(
+		{
+			url: '/signup',
+			data: $('form').serialize(),
+			type: 'POST',
+			success: function(response)
+			{
+				console.log(response);
+			},
+			error: function(error)
+			{
+				console.log(error);
+			}
+		});
+	});
+});
